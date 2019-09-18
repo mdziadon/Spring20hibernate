c2b4220144dcb7ed5f4fc9f2a0bda0f04c61da52
Marcin Dziadon <marcin.dziadon@coderslab.pl>
2019-09-18 11:16:14 +0200

    Zadania walidacja dzien 3

--- src/main/java/pl/coderslab/validation/ValidationController.java
+++ src/main/java/pl/coderslab/validation/ValidationController.java
@@ -2,12 +2,15 @@ package pl.coderslab.validation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.coderslab.book.Book;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 @Controller
@@ -21,15 +24,19 @@ public class ValidationController {
     }
 
     @GetMapping(value = "/validate", produces = "text/html; charset=utf-8")
-    @ResponseBody
-    public String validate() {
+    public String validate(Model model) {
         Book book = new Book();
 
         Set<ConstraintViolation<Book>> errors = validator.validate(book);
-        StringBuilder sb = new StringBuilder();
+        List<FieldError> errorList = new ArrayList<>();
+
         for (ConstraintViolation<Book> error : errors) {
-            sb.append(error.getPropertyPath()).append(": ").append(error.getMessage()).append("<br>");
+            String path = error.getPropertyPath().toString();
+            String message = error.getMessage();
+            errorList.add(new FieldError(path, message));
         }
-        return sb.toString();
+
+        model.addAttribute("errors", errorList);
+        return "errors";
     }
 }
